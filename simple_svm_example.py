ce0aeb18d205c5260835ec97b0c78707023e49ea
Thomas Cohn <cohnt@umich.edu>
2018-05-24 14:30:14 -0400

    Made it animate and stuff. I actually did this like a week ago, but forgot to commit!

--- simple_svm_example.py
+++ simple_svm_example.py
@@ -3,22 +3,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.datasets import make_classification
 
 rs = np.random.RandomState(1234)
 
-# Generate some fake data.
-n_samples = 200
-# X is the input features by row.
-X = np.zeros((200,3))
-X[:n_samples/2] = rs.multivariate_normal( np.ones(3), np.eye(3), size=n_samples/2)
-X[n_samples/2:] = rs.multivariate_normal(-np.ones(3), np.eye(3), size=n_samples/2)
-# Y is the class labels for each row of X.
-Y = np.zeros(n_samples); Y[n_samples/2:] = 1
+X, Y = make_classification(n_features=3, n_redundant=1, random_state=0)
+
+# # Generate some fake data.
+# n_samples = 200
+# # X is the input features by row.
+# X = np.zeros((200,3))
+# X[:n_samples/2] = rs.multivariate_normal( np.ones(3), np.eye(3), size=n_samples/2)
+# X[n_samples/2:] = rs.multivariate_normal(-np.ones(3), np.eye(3), size=n_samples/2)
+# # Y is the class labels for each row of X.
+# Y = np.zeros(n_samples); Y[n_samples/2:] = 1
 
 # Fit the data with an svm
-svc = SVC(kernel='linear')
-svc.fit(X,Y)
+# svc = SVC(kernel='linear')
+# svc.fit(X,Y)
+
+svc = LinearSVC(random_state=0)
+svc.fit(X, Y)
 
 # The equation of the separating plane is given by all x in R^3 such that:
 # np.dot(svc.coef_[0], x) + b = 0. We should for the last coordinate to plot
